14ffcd8742fb5168b6c7c6c95e895542e3691ff0
개좆밥 <gaejotbab@gmail.com>
2021-03-27 21:30:29 +0900

    로깅 인프라 추가하고 인자 확인

--- src/main/java/dev/gaejotbab/takealook/Application.java
+++ src/main/java/dev/gaejotbab/takealook/Application.java
@@ -1,6 +1,12 @@
 package dev.gaejotbab.takealook;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
+    private static final int EXIT_FAILURE = 1;
+    private final Logger logger = LoggerFactory.getLogger(Application.class);
+
     private final String[] args;
 
     public Application(String[] args) {
@@ -8,7 +14,18 @@ public class Application {
     }
 
     public void run() {
-        
+        if (args.length < 1) {
+            logger.error("인자 갯수가 부족합니다.");
+            System.exit(EXIT_FAILURE);
+        }
+
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            logger.error("포트가 잘못되었습니다.");
+            System.exit(EXIT_FAILURE);
+        }
     }
 
     public static void main(String[] args) {
